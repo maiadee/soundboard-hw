66419629c53f88f1bb02d24246f1eaca95fb1417
Maiadee <maiadeemmony@hotmail.co.uk>
2024-12-09 16:48:29 +0000

    work in progress

---
+++ scripts/app.js
@@ -0,0 +1,138 @@
+// const workItButton = document.querySelector("#workIt");
+// const workItAudio = document.querySelector("#workItAudio");
+
+// workItButton.addEventListener("hover", () => {
+//   workItAudio.currentTime = 0;
+//   workItAudio.play();
+// });
+
+// loop needed instead
+
+const workItElement = document.querySelector("#workIt");
+const workItAudio = document.querySelector("#workItAudio");
+
+workItElement.addEventListener("mouseover", () => {
+  workItAudio.currentTime = 0;
+  workItAudio.play();
+  // workItElement.style.backgroundImage = "url('head.gif')";
+});
+
+const makeItElement = document.querySelector("#makeIt");
+const makeItAudio = document.querySelector("#makeItAudio");
+
+makeItElement.addEventListener("mouseover", () => {
+  makeItAudio.currentTime = 0;
+  makeItAudio.play();
+});
+
+const doItElement = document.querySelector("#doIt");
+const doItAudio = document.querySelector("#doItAudio");
+
+doItElement.addEventListener("mouseover", () => {
+  doItAudio.currentTime = 0;
+  doItAudio.play();
+});
+
+const makesUsElement = document.querySelector("#makesUs");
+const makesUsAudio = document.querySelector("#makesUsAudio");
+
+makesUsElement.addEventListener("mouseover", () => {
+  makesUsAudio.currentTime = 0;
+  makesUsAudio.play();
+});
+
+const harderElement = document.querySelector("#harder");
+const harderAudio = document.querySelector("#harderAudio");
+
+harderElement.addEventListener("mouseover", () => {
+  harderAudio.currentTime = 0;
+  harderAudio.play();
+});
+
+const betterElement = document.querySelector("#better");
+const betterAudio = document.querySelector("#betterAudio");
+
+betterElement.addEventListener("mouseover", () => {
+  betterAudio.currentTime = 0;
+  betterAudio.play();
+});
+
+const fasterElement = document.querySelector("#faster");
+const fasterAudio = document.querySelector("#fasterAudio");
+
+fasterElement.addEventListener("mouseover", () => {
+  fasterAudio.currentTime = 0;
+  fasterAudio.play();
+});
+
+const strongerElement = document.querySelector("#stronger");
+const strongerAudio = document.querySelector("#strongerAudio");
+
+strongerElement.addEventListener("mouseover", () => {
+  strongerAudio.currentTime = 0;
+  strongerAudio.play();
+});
+
+const moreThanElement = document.querySelector("#moreThan");
+const moreThanAudio = document.querySelector("#moreThanAudio");
+
+moreThanElement.addEventListener("mouseover", () => {
+  moreThanAudio.currentTime = 0;
+  moreThanAudio.play();
+});
+
+const hourElement = document.querySelector("#hour");
+const hourAudio = document.querySelector("#hourAudio");
+
+hourElement.addEventListener("mouseover", () => {
+  hourAudio.currentTime = 0;
+  hourAudio.play();
+});
+
+const ourElement = document.querySelector("#our");
+const ourAudio = document.querySelector("#ourAudio");
+
+ourElement.addEventListener("mouseover", () => {
+  ourAudio.currentTime = 0;
+  ourAudio.play();
+});
+
+const neverElement = document.querySelector("#never");
+const neverAudio = document.querySelector("#neverAudio");
+
+neverElement.addEventListener("mouseover", () => {
+  neverAudio.currentTime = 0;
+  neverAudio.play();
+});
+
+const everElement = document.querySelector("#ever");
+const everAudio = document.querySelector("#everAudio");
+
+everElement.addEventListener("mouseover", () => {
+  everAudio.currentTime = 0;
+  everAudio.play();
+});
+
+const afterElement = document.querySelector("#after");
+const afterAudio = document.querySelector("#afterAudio");
+
+afterElement.addEventListener("mouseover", () => {
+  afterAudio.currentTime = 0;
+  afterAudio.play();
+});
+
+const workIsElement = document.querySelector("#workIs");
+const workIsAudio = document.querySelector("#workIsAudio");
+
+workIsElement.addEventListener("mouseover", () => {
+  workIsAudio.currentTime = 0;
+  workIsAudio.play();
+});
+
+const overElement = document.querySelector("#over");
+const overAudio = document.querySelector("#overAudio");
+
+overElement.addEventListener("mouseover", () => {
+  overAudio.currentTime = 0;
+  overAudio.play();
+});
